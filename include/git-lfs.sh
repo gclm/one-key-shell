dec9b3425dbf764da7e7459ce8db104568ebc755
gclm <1719982754@qq.com>
2020-03-18 15:47:37 +0800

    fix(发布 1.0.1 版本):
    1. 解决 axel 和 mwget 脚本的bug
    2. 优化 git 安装成功数据
    3. 增加 git-lfs 安装脚本

--- include/git-lfs.sh
+++ include/git-lfs.sh
@@ -1,17 +1,29 @@
 #!/usr/bin/env bash
 
 install(){
-    git_shell=`command -v git`
+    yum install -y epel-release
+    curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | bash
+    yum install -y git-lfs
+    echo "git lfs 安装完成"
+    git lfs install
+}
+
+main(){
+git_shell=`command -v git`
     if [[ ${git_shell} == "/usr/local/git/bin/git" || ${git_shell} == "/usr/bin/git" ]]; then
-        yum install -y epel-release
-        curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.rpm.sh | bash
-        yum install -y git-lfs
-        echo "git lfs 安装完成"
-        git lfs install
+       install
     else
-        echo "请安装 git 后在进行安装 Git lfs !!!!"
-		exit
+		echo "no exists git，是否安装 git 后在进行安装 Git lfs ？[Y/n]"
+		read -p "(默认: y):" yn
+		[[ -z "${yn}" ]] && yn="y"
+		if [[ ${yn} == [Yy] ]]; then
+			curl https://gitee.com/gclm/one-key-linux/raw/master/include/git.sh | bash
+            install
+		else
+			echo && echo "	已取消..." && echo
+		fi
+        sleep 5s
     fi
 }
 
-install
+main
