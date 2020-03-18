494584f03bf923a9f8d08d525921a6162b0c0d39
gclm <1719982754@qq.com>
2020-03-18 15:08:49 +0800

    fix(修改 mwget 脚本):

--- include/mwget.sh
+++ include/mwget.sh
@@ -1,46 +1,11 @@
 #!/usr/bin/env bash
 
-
-#==================基础配置 start ============================
-
-# 字体样式
-Green_font_prefix="\033[32m" && Red_font_prefix="\033[31m" && Green_background_prefix="\033[42;37m" && Red_background_prefix="\033[41;37m" && Font_color_suffix="\033[0m"
-Info="${Green_font_prefix}[信息]${Font_color_suffix}"
-Error="${Red_font_prefix}[错误]${Font_color_suffix}"
-Tip="${Green_font_prefix}[注意]${Font_color_suffix}"
-
-# 安装路径
-base_path="/usr/local"
-java_Path="$base_path/java"
-git_path="$base_path/git"
-nexus_path="$base_path/nexus"
-soft_path="/opt/software"
-module_path="/opt/module"
-
-# 版本
-shell_version="0.0.4"
-maven_version="3.6.2"
-jdk_version="221"
-tomcat_version="9.0.19"
-gradle_version=""
-nexus_version="3.19.1-01"
-mysql_version=""
-git_version="2.9.5"
-mwget_version="0.1.0"
-
-# 远程安装包地址
-coding="https://dev.tencent.com/u/gclm/p/resources/git/raw/master"
-
-#==================基础配置 end =============================
-
 install(){
 
-# wget https://dev.tencent.com/u/gclm/p/resources/git/raw/master/mwget/mwget_0.1.0.orig.tar.bz2
-
-wget ${coding}/mwget/mwget_0.1.0.orig.tar.bz2
+wget https://gclm.coding.net/p/yum/d/yum/git/raw/master/mwget_0.1.0.orig.tar.bz2
 tar -xjvf mwget_0.1.0.orig.tar.bz2
 cd mwget_0.1.0.orig
-./configure
+./configure --prefix=/usr/local/mwget
 yum install intltool -y
 make && make install
 
