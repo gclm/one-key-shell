37d9a76cf3bd7ed679f69fbf99974806344f1f82
gclm <1719982754@qq.com>
2020-03-18 16:32:21 +0800

    fix(脚本支持 centos8。优化版本效验问题):

--- include/init.sh
+++ include/init.sh
@@ -1,40 +1,44 @@
 #!/usr/bin/env bash
 
-main(){
 # 判断系统是否是centos和 root 用户
-wget https://gitee.com/gclm/one-key-linux/raw/master/include/system.sh && chmod +x system.sh
-. system.sh
+main(){
 
-yum install -y curl wget vim
-echo "安装 curl wget vim success"
+    shell_file=$(ls | grep system.sh)
+    if [ ! -f "$shell_file" ]; then
+        wget https://gitee.com/gclm/one-key-linux/raw/master/include/system.sh && chmod +x system.sh
+    fi
+    . system.sh
 
-# 修改DNS  vi /etc/resolv.conf
-echo "nameserver 8.8.8.8" >> /etc/resolv.conf
-echo "nameserver 114.114.114.114" >> /etc/resolv.conf
+    yum install -y curl wget vim
+    echo "安装 curl wget vim success"
 
-if [[ ${version} == "7"  ]]; then
-	# 更换镜像为阿里云镜像
-    cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-    wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-    wget -O /etc/yum.repos.d/epel-7.repo  http://mirrors.aliyun.com/repo/epel-7.repo
-    sed -i 's#http://mirrors.cloud.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/CentOS-Base.repo
-    sed -i 's#http://mirrors.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/CentOS-Base.repo
-    sed -i 's#http://mirrors.cloud.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/epel-7.repo
-    yum clean all
-    yum makecache
-fi
+    # 修改DNS  vi /etc/resolv.conf
+    echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+    echo "nameserver 114.114.114.114" >> /etc/resolv.conf
 
-# 更新 yum 组件版本
-yum -y update
-yum -y groupinstall 'Development Tools'
+    if [[ ${version} == "7"  ]]; then
+        # 更换镜像为阿里云镜像
+        cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+        wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+        wget -O /etc/yum.repos.d/epel-7.repo  http://mirrors.aliyun.com/repo/epel-7.repo
+        sed -i 's#http://mirrors.cloud.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/CentOS-Base.repo
+        sed -i 's#http://mirrors.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/CentOS-Base.repo
+        sed -i 's#http://mirrors.cloud.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/epel-7.repo
+        yum clean all
+        yum makecache
+    fi
 
-# 修改控制台
-curl https://gitee.com/gclm/one-key-linux/raw/master/include/update-hostname.sh | bash
+    # 更新 yum 组件版本
+    yum -y update
+    yum -y groupinstall 'Development Tools'
 
-# 增加交换2G交换分区
-curl https://gitee.com/gclm/one-key-linux/raw/master/include/swap.sh | bash
-rm -rf system.sh
-reboot
+    # 修改控制台
+    curl https://gitee.com/gclm/one-key-linux/raw/master/include/update-hostname.sh | bash
+
+    # 增加交换2G交换分区
+    curl https://gitee.com/gclm/one-key-linux/raw/master/include/swap.sh | bash
+    rm -rf system.sh
+    reboot
 
 }
 
