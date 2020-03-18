912bb80673d11d998b6dde2bd27746d097b34813
gclm <1719982754@qq.com>
2020-03-18 16:36:18 +0800

    feat(增加 nginx.sh centos8 支持):

--- include/nginx.sh
+++ include/nginx.sh
@@ -3,12 +3,18 @@
 
 install(){
 
+shell_file=$(ls | grep system.sh)
+if [ ! -f "$shell_file" ]; then
+    wget https://gitee.com/gclm/one-key-linux/raw/master/include/system.sh && chmod +x system.sh
+fi
+. system.sh
+
 rm -rf /etc/yum.repos.d/nginx.repo
 cd /etc/yum.repos.d
 sudo tee /etc/yum.repos.d/nginx.repo <<-'EOF'
 [nginx]
 name=nginx repo
-baseurl=http://nginx.org/packages/centos/7/$basearch/
+baseurl=http://nginx.org/packages/centos/${version}/$basearch/
 gpgcheck=0
 enabled=1
 EOF
