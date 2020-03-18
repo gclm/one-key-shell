24595a1b55dfe79f8e6338d5799006ccbb38b364
gclm <1719982754@qq.com>
2020-03-18 11:53:45 +0800

    fix(修改 aliyun centos 7 镜像路径错误):

--- include/init.sh
+++ include/init.sh
@@ -1,33 +1,29 @@
 #!/usr/bin/env bash
 
-# 初始化环境
-yum(){
-cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
-wget -P /etc/yum.repos.d/ http://mirrors.aliyun.com/repo/epel-7.repo
-yum clean all
-yum makecache
-}
-
-install(){
-# 更新 yum 组件版本
-yum -y update
-
-# 安装基础组件
-yum install -y curl wget vim
-yum -y groupinstall 'Development Tools'
-
-echo "安装 curl wget vim success"
-}
-
 main(){
 # 判断系统是否是centos和 root 用户
 curl https://gitee.com/gclm/one-key-linux/raw/master/include/system.sh | bash
 
-# 更换镜像为阿里云镜像
-yum
+yum install -y curl wget vim
+echo "安装 curl wget vim success"
 
-install
+# 修改DNS  vi /etc/resolv.conf
+echo "nameserver 8.8.8.8" >> /etc/resolv.conf
+echo "nameserver 114.114.114.114" >> /etc/resolv.conf
+
+# 更换镜像为阿里云镜像
+cp /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
+wget -O /etc/yum.repos.d/CentOS-Base.repo http://mirrors.aliyun.com/repo/Centos-7.repo
+wget -O /etc/yum.repos.d/epel-7.repo  http://mirrors.aliyun.com/repo/epel-7.repo
+sed -i 's#http://mirrors.cloud.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/CentOS-Base.repo
+sed -i 's#http://mirrors.cloud.aliyuncs.com#http://mirrors.aliyun.com#g' /etc/yum.repos.d/epel-7.repo
+yum clean all
+yum makecache
+
+# 更新 yum 组件版本
+yum -y update
+
+yum -y groupinstall 'Development Tools'
 
 # 修改控制台
 curl https://gitee.com/gclm/one-key-linux/raw/master/include/update-hostname.sh | bash
