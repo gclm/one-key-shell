c151cdc0decedbcdee241d8ae0ffe66c1a400cf3
gclm <1719982754@qq.com>
2020-04-12 20:09:09 +0800

    feat(发布 v1.1.1 版本): 更新日志
    1. 优化maridb 安装脚本

--- include/mariadb-10.4.sh
+++ include/mariadb-10.4.sh
@@ -1,7 +1,6 @@
 #!/usr/bin/env bash
 
-# 安装 mariadb 10.4 二进制安装成功
-Install_MariaDB104(){
+init(){
 # 安装 mariadb.repo
 cat > /etc/yum.repos.d/mariadb.repo << EOF
 # MariaDB 10.4 CentOS repository list - created 2020-02-17 06:03 UTC
@@ -16,6 +15,12 @@ EOF
 # 更换 mariadb 为国内源（建议使用 HTTPS）
 sed -i 's#yum\.mariadb\.org#mirrors.ustc.edu.cn/mariadb/yum#' /etc/yum.repos.d/mariadb.repo
 sed -i 's#http://mirrors\.ustc\.edu\.cn#https://mirrors.ustc.edu.cn#g' /etc/yum.repos.d/mariadb.repo
+}
+
+# 安装 mariadb 10.4 二进制安装成功
+main(){
+
+init
 
 # 安装 mariadb
 yum install MariaDB-server MariaDB-client -y
@@ -23,3 +28,5 @@ yum install MariaDB-server MariaDB-client -y
 # https://blog.csdn.net/zhezhebie/article/details/73549741
 # https://mirrors.ustc.edu.cn/help/mariadb.html
 }
+
+main
