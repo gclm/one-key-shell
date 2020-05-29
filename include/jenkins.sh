a7df16a14772eb9f511ebd52ba77c21ccef5faef
gclm <1719982754@qq.com>
2020-05-30 02:06:27 +0800

    fix(增加 jenkins 离线安装)

--- include/jenkins.sh
+++ include/jenkins.sh
@@ -1,8 +1,13 @@
 #!/usr/bin/env bash
 
-
 install(){
 echo -e "开始安装 Jenkins"
+wget  https://mirrors.huaweicloud.com/jenkins/redhat-stable/jenkins-2.222.4-1.1.noarch.rpm
+rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
+rpm -ivh jenkins-2.222.4-1.1.noarch.rpm
+}
+
+install2(){
 wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
 yum install -y jenkins
