cdd64d15a702eef01aeed15673a2aefef7af3719
孤城落寞 <gclmit@163.com>
2023-03-07 14:14:25 +0800

    Update docker.sh

--- linux/docker.sh
+++ linux/docker.sh
@@ -1,9 +1,12 @@
+echo "删除旧版docker"
 yum remove -y docker docker-common docker-selinux docker-engine
+echo "更新docker镜像并下载docker"
 yum install -y yum-utils device-mapper-persistent-data lvm2
 wget -O /etc/yum.repos.d/docker-ce.repo https://repo.huaweicloud.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
 yum install -y docker-ce
 kill -TERM 1
+echo "配置docker国内镜像源"
 sudo mkdir -p /etc/docker
 sudo tee /etc/docker/daemon.json <<-'EOF'
 {
