e3269f102f675c8ca324a820f9217d4a820cf322
孤城落寞 <gclmit@163.com>
2023-03-07 14:12:46 +0800

    Update docker.sh

--- linux/docker.sh
+++ linux/docker.sh
@@ -4,3 +4,14 @@ wget -O /etc/yum.repos.d/docker-ce.repo https://repo.huaweicloud.com/docker-ce/l
 yum makecache fast
 yum install -y docker-ce
 kill -TERM 1
+sudo mkdir -p /etc/docker
+sudo tee /etc/docker/daemon.json <<-'EOF'
+{
+  "registry-mirrors": [ 
+     "https://hub-mirror.c.163.com",
+     "https://mirror.baidubce.com"
+   ]
+}
+EOF
+sudo systemctl daemon-reload
+sudo systemctl restart docker
