33ae5e8fb2cc03e128529c81386fff9e096a1f9c
孤城落寞 <gclmit@163.com>
2023-03-07 14:05:22 +0800

    Update docker.sh

--- linux/docker.sh
+++ linux/docker.sh
@@ -3,3 +3,4 @@ yum install -y yum-utils device-mapper-persistent-data lvm2
 wget -O /etc/yum.repos.d/docker-ce.repo https://repo.huaweicloud.com/docker-ce/linux/centos/docker-ce.repo
 yum makecache fast
 yum install -y docker-ce
+kill -TERM 1
