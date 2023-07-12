ccda0a2057dbe7d37132deb3968ec738f9834018
孤城落寞 <gclmit@163.com>
2023-07-12 11:53:13 +0800

    Delete centos8.sh

--- linux/centos8.sh
+++
@@ -1,4 +0,0 @@
-cp -a /etc/yum.repos.d/CentOS-Base.repo /etc/yum.repos.d/CentOS-Base.repo.bak
-wget -O /etc/yum.repos.d/CentOS-Base.repo https://repo.huaweicloud.com/repository/conf/CentOS-8-reg.repo
-yum clean all
-yum makecache
